d52000df1d65d1eea94638b23b5f8767890b8f68
Bill Beesley <bill.beesley@williamhill.com>
2018-05-09 12:43:42 +0200

    more nick cage

--- src/main/server.js
+++ src/main/server.js
@@ -8,6 +8,7 @@ import config from './config';
 import {
     health,
     dank,
+    cage,
 } from './routes';
 
 const router = new Router();
@@ -21,6 +22,7 @@ const init = async (): Promise<void> => {
     log.info('Server has started. Initializing routes.');
     router.get('/healthcheck', health);
     router.get('/dank.gif', dank);
+    router.get('/nick-cage.gif', cage);
     const app = new Koa();
     app.use(koaBunyanLogger());
     app.use(koaBunyanLogger.requestIdContext());
